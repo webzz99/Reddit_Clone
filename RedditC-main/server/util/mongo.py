b3dd89f4f2c273e605e8b3fd55e841004ae7675d
pipiikiler <su.win8188@gmail.com>
2024-04-18 13:34:37 +0100

    change mongo client

--- RedditC-main/server/util/mongo.py
+++ RedditC-main/server/util/mongo.py
@@ -3,11 +3,11 @@ from pymongo import MongoClient
 import os  # Import the os module to access environment variables
 
 # Use an environment variable for the MongoDB URI, or default to a local MongoDB instance
-MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://127.0.0.1:27017/redc')
-MONGO_CLIENT = MongoClient(MONGO_URI)
+# MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://127.0.0.1:27017/redc')
+# MONGO_CLIENT = MongoClient(MONGO_URI)
+MONGO_CLIENT = MongoClient('mongodb+srv://volkan1990:volkan1990@redc.7bbrtch.mongodb.net/?retryWrites=true&w=majority&appName=REDC')
 REDDITC_DB = MONGO_CLIENT["redditc_db"]
 
-client = MongoClient('mongodb+srv://volkan1990:volkan1990@redc.7bbrtch.mongodb.net/?retryWrites=true&w=majority&appName=REDC')
 
 USER_COLLECTION = REDDITC_DB.user
 POST_COLLECTION = REDDITC_DB.post
